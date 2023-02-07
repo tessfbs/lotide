e25a52cebcf615390b5dea778b6a1835604ebbae
tessfbs <tessfbs@gmail.com>
2023-02-06 22:08:46 -0500

    function added

---
+++ assertEqual.js
@@ -0,0 +1,31 @@
+//The console.assert() method writes an error message to the console if the assertion is false. If the assertion is true, nothing happens.
+
+//assert(assertion, obj1)
+//assert(assertion, obj1, obj2)
+//assert(assertion, obj1, obj2, /* … ,*/ objN)
+
+//assert(assertion, msg)
+//assert(assertion, msg, subst1)
+//assert(assertion, msg, subst1, /* … ,*/ substN) 
+
+
+//Parameters 
+  //assertion = Any boolean expression. If the assertion is false, the message is written to the console.
+
+  //obj1 … objN = A list of JavaScript objects to output. The string representations of each of these objects are appended together in the order listed and output.
+
+  //msg = A JavaScript string containing zero or more substitution strings.
+
+  //subst1 … substN = JavaScript objects with which to replace substitution strings within msg. This parameter gives you additional control over the format of the output.
+
+
+  const assertEqual = function(actual, expected){
+    if(actual === expected){
+      console.log("🔴🔴🔴 Assertion Passed: " + actual + " === " + expected)
+    }else{
+      console.log("🟢🟢🟢 Assertion Failed: " + actual + " !== " + expected)
+    }
+  };
+
+  assertEqual("Lighthouse Labs", "Bootcamp");
+  assertEqual(1, 1);
