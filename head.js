b0a9280f184b2d555f19d33bf1c4d2b8d7cde3e0
tessfbs <tessfbs@gmail.com>
2023-02-07 21:31:16 -0500

    .

--- head.js
+++ head.js
@@ -14,4 +14,4 @@ const head = function(array){
 }
 
 assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");÷
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
